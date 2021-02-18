40f2dadb17aee940330d7763f94cfdd4c3c4bff0
Felipe Matarazzo <felipemps@protonmail.com>
2021-02-18 17:03:16 -0300

    looking good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+puts 'puxando os ingredientes...'
+
+ingredients['drinks'].each do |i|
+  ingredient = Ingredient.create(
+    name: i['strIngredient1']
+  )
+  puts "criando ingrediente de id #{ingredient.id}"
+end
+
+puts '...ingredientes prontos!'
