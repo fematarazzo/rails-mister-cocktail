ce69850bcbfdfabd4f6fd32ed9298fd060cfb496
Felipe Matarazzo <felipemps@protonmail.com>
2021-02-19 11:47:52 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: %i[new create]
   end
+
   resources :doses, only: :destroy
+  root to: 'cocktails#index'
 end
